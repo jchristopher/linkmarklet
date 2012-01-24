7753619293d1274c335a18fe721196644f8221e5
Jonathan Christopher <jonathan@irontoiron.com>
2012-01-23 19:03:51 -0500

    Added default title for Alternate bookmarklet

--- remove-this.php
+++ remove-this.php
@@ -5,9 +5,17 @@ require_once( preg_replace( "/wp-content.*/", "/wp-admin/includes/admin.php", __
 ob_end_clean();
 
 ?>
-<ol>
-    <li>Bookmark this page</li>
-    <li>Edit the bookmark</li>
-    <li>Remove <strong><?php echo LINKMARKLET_URL . '/remove-this.php?'; ?></strong> and leave everything else</li>
-    <li>Save your bookmarklet</li>
-</ol>
+<html>
+    <head>
+        <title>Linkmarklet</title>
+    </head>
+    <body>
+    <ol>
+        <li>Bookmark this page</li>
+        <li>Edit the bookmark</li>
+        <li>Remove <strong><?php echo LINKMARKLET_URL . '/remove-this.php?'; ?></strong> and leave everything else</li>
+        <li>Save your bookmarklet</li>
+    </ol>
+
+    </body>
+</html>
